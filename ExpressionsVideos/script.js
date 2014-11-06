9a3dbb543d6a340ba51b91f9e94cd1a811eff881
DesireeAnn <desireeacriado@gmail.com>
2014-11-06 12:51:08 -0800

    Arithmetic Operators
    Expressions videos - Arithmetic Operators

--- ExpressionsVideos/script.js
+++ ExpressionsVideos/script.js
@@ -1,8 +1,18 @@
 //Expressions
-
+/*
 //define variable a with 2
 var a = 2;
 var b;
 //add 3 to a
 b = a + 3;
 console.log(b);
+*/
+
+//Arithmetic Operators
+
+//area of the triangle is half the width times the height
+var width = 4;
+var height = 5;
+var area = width * height/2;
+
+console.log(area);
