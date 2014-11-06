2dfd43854bc378de776166e847d3fe17f25c8b37
DesireeAnn <desireeacriado@gmail.com>
2014-11-06 14:59:25 -0800

    Calculate Total
    Calculating the total number of screens needed; still need to adjust
    total screens variable equation

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -2,3 +2,27 @@
 11-06-14
 Expressions - Industry
  */
+//Calculating how many screens I will need next week for my current job as a screen printer.
+
+//Ask the user if the design is front only or front and back.
+var design = prompt("Let's see how many silkscreens Desi needs to print this week! \nDoes the design require printing on 1 side or 2 sides?");
+//Console.log out the response
+console.log(design);
+
+//Ask the user how many colors are in the design.
+var color = prompt("How many colors are in the design?")
+//Console.log out the response
+console.log(color);
+
+//Ask the user how many designs need to be printed.
+var designNo = prompt("How many designs need to be printed?");
+//Console.log out the response
+console.log(designNo);
+
+//Calculate total number of screens
+var totalScreens = (design + color) * designNo;
+
+//Present results to user
+alert("Desi will need "+ totalScreens + " screens to print this week.");
+
+
