6ac80d2ee8c258e98bcaaacf624d85102cfbf50d
DesireeAnn <desireeacriado@gmail.com>
2014-11-06 12:14:37 -0800

    Finished Values and Variables
    completed the values and variables videos

---
+++ Values_Variables/Arrays/script.js
@@ -0,0 +1,11 @@
+//Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
+
+muppetNames[4] = "Fozzy";
+
+var num = 2;
+
+console.log(muppetNames[num]);
+
+
---
+++ Values_Variables/String And Boolean Variables/script.js
@@ -0,0 +1,11 @@
+//Variables of Different Types, Numbers, Booleans, and Strings
+
+var age = 26; // Number variable
+
+var isStudent = false; //Boolean Variable
+
+var firstName = "Desiree Ann";
+
+var phrase = 'I don\'t know \n You\'ll have to speak with the professor';
+
+
