f8d98fdf5c33832a4dcbb701497b64f64edefb79
DesireeAnn <desireeacriado@gmail.com>
2014-11-06 22:12:50 -0800

    Worksheet
    Worksheet formatting

--- Expressions_worksheet/script.js
+++ Expressions_worksheet/script.js
@@ -6,6 +6,22 @@ Expressions Worksheet
 //Dog Years
 //Calculate how old Sparky the dog is in dog years based on his actual age
 
-var realAge = x
+var realAge = 2
+const 7 = 7
+console.log(dogYears);
+console.log("Sparky is " + realAge + " human years old which is " + dogYears + " in dog years.");
 
-console.log("Sparky is " + + " human years old which is " + + " in dog years.");
+
+//Slice of Pie pt.1
+//Calculate how much pizza each student will get at the party.
+
+//Number of slices per pizza
+//number of people at the party
+//number of pizzas ordered
+
+
+//Slice of Pie pt. 2
+
+//Average Shopping Bill
+
+//Discounts
