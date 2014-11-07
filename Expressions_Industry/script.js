5ee906389e470b5298a6d86241f603bda151469d
DesireeAnn <desireeacriado@gmail.com>
2014-11-06 16:05:23 -0800

    Finished calculations
    completed calculations and prompts for total number of screens.

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -5,12 +5,12 @@ Expressions - Industry
 //Calculating how many screens I will need next week for my current job as a screen printer.
 
 //Ask the user if the design is front only or front and back.
-var design = prompt("Let's see how many silkscreens Desi needs to print this week! \nDoes the design require printing on 1 side or 2 sides?");
+var designSide = prompt("Let's see how many silkscreens Desi needs to print this week! \nDoes the design require printing on 1 side or 2 sides?");
 //Console.log out the response
-console.log(design);
+console.log(designSide);
 
 //Ask the user how many colors are in the design.
-var color = prompt("How many colors are in the design?")
+var color = prompt("How many colors are on each side of the design?");
 //Console.log out the response
 console.log(color);
 
@@ -20,7 +20,7 @@ var designNo = prompt("How many designs need to be printed?");
 console.log(designNo);
 
 //Calculate total number of screens
-var totalScreens = (design + color) * designNo;
+var totalScreens = designSide * color * designNo;
 
 //Present results to user
 alert("Desi will need "+ totalScreens + " screens to print this week.");
