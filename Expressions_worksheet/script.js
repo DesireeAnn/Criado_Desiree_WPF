66d1eb6fbd21fc7d0c42365abbc43c1a7c6d48d7
DesireeAnn <desireeacriado@gmail.com>
2014-11-06 21:56:47 -0800

    Worksheet
    Problem 1

--- Expressions_worksheet/script.js
+++ Expressions_worksheet/script.js
@@ -3,5 +3,9 @@
 Expressions Worksheet
  */
 
+//Dog Years
+//Calculate how old Sparky the dog is in dog years based on his actual age
 
+var realAge = x
 
+console.log("Sparky is " + + " human years old which is " + + " in dog years.");
