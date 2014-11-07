189632c29eed089ef34c3662446268adc89f996c
DesireeAnn <desireeacriado@gmail.com>
2014-11-06 19:50:11 -0800

    Expressions_Industry all console updates
    Fixed all console.log printouts to be in complete sentences

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -10,18 +10,18 @@ alert("Let's see how many silkscreens Desi needs to screenprint this week!");
 //Ask the user if the design is front only or front and back.
 var designSide = prompt("Does the design require printing on 1 side or 2 sides?");
 //Console.log out the response
-console.log(designSide);
+console.log("The design has " + designSide + " sides.");
 
 //Ask the user how many colors are in the design.
 var color = prompt("How many colors are on each side of the design?");
 //Console.log out the response
-console.log(color);
+console.log("Each side of the design has " + color+ " colors.");
 
 //Ask the user how many designs need to be printed.
 var designNo = prompt("How many designs need to be printed?");
 
 //Console.log out the response
-console.log(designNo);
+console.log(designNo + " designs need to be printed.");
 
 //Calculate total number of screens
 var totalScreens = designSide * designNo + color * designNo;
