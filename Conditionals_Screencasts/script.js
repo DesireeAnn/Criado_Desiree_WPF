08c2be6084cf3cc4c982e95554617df6c0d88f88
DesireeAnn <desireeacriado@gmail.com>
2014-11-11 07:17:03 -0800

    Relational Expressions
    Second screencast video

--- Conditionals_Screencasts/script.js
+++ Conditionals_Screencasts/script.js
@@ -1,6 +1,6 @@
 //Conditional Logic
 
-var oldEnough = false;
+/*var oldEnough = false;
 
 //if the child is old enough, print to the console "you can ride!"
 if(oldEnough){
@@ -8,3 +8,15 @@ if(oldEnough){
     console.log("You can ride the coaster!");
 }
 console.log("What comes after");
+*/
+
+//Relational Expressions
+
+var kidHeight = 40;
+var minHeight = 48;
+
+//if the child is over the height of 48 inches
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
