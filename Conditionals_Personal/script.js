9ab9985b8309be42b891e6c96ddf859af0801ac6
DesireeAnn <desireeacriado@gmail.com>
2014-11-12 22:02:39 -0800

    General Outline
    Determining basics for Conditionals - Personal assignment

---
+++ Conditionals_Personal/script.js
@@ -0,0 +1,19 @@
+/* Desiree Criado
+11/12/14
+Conditionals - Personal
+ */
+//Determine how many times the animals (cats and dogs) need to be fed
+//Greet user
+alert("Thank you for feeding our pets while we're away!");
+//Record whether or not the cats already ate their first serving
+prompt("Do the cats currently have food in their bowl?");
+//Record whether or not the dogs already ate their first serving
+prompt("Do the dogs currently have food in their bowl?");
+//If both the dogs and cats have empty bowls
+alert("You will need to feed the cats and the dogs twice!")
+//If both the cats and dogs have food in their bowls
+alert("You will need to feed the cats and the dogs once!")
+//If the cats have food in their bowls but the dogs do not
+alert("You will need to feed the cats once and the dogs twice!");
+//If the cats do not have food in their bowls but the dogs do
+alert("You will need to feed the cats twice and the dogs once!");
--- Conditionals_Worksheet/script.js
+++ Conditionals_Worksheet/script.js
@@ -1,16 +1,29 @@
 /* Desiree Criado
 11/12/14
 Conditionals Worksheet
- */
+
 
 //Group 1 - Last Chance for Gas!
-
+//Define givens: MPG
 var mpg = 40;
+//Gas Tank's total capacity
 var totalGas = 15;
+//Percentage of gas currently in the tank
 var currentGas = totalGas/5;
-
+//If the amount of gas in the car times the MPG is GREATER than or EQUAL to 200
 if(currentGas * mpg >= 200){
     console.log("Yes, you can make it without stopping for gas!");
+    //If the amount of gas in the car times the MPG is less than 200
 }else{
     console.log("You only have " + Number(currentGas) + " gallons of gas in your tank, better get gas now while you can!");
+}
+*/
+
+//Check the login
+//Define givens
+var username = true;
+var password = true;
+
+if(username = true){
+    console.log("Welcome, "+)
 }
