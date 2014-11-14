5bb4939aef6a4da6c86f3778970b703e3df02407
DesireeAnn <desireeacriado@gmail.com>
2014-11-13 16:35:39 -0800

    Completed Wacky
    Assignment completed; still need to be reviewed with the rubric

--- Conditionals_Wacky/script.js
+++ Conditionals_Wacky/script.js
@@ -4,22 +4,32 @@ Desiree Criado
 Conditionals - Wacky
  */
 
-//
+//Find out whether your facebook friends post mostly celebrity news, political news,
+// a little of both, or neither
 alert("What kind of posts fill your facebook feed?");
-
+//Define celebrity variable; prompt user
 var celeb = prompt("Have you seen Kim Kardashian's latest ad?");
-
+//Check for user input
+if(celeb === ""){
+    var celeb = prompt("Looks like we didn't catch that answer! Have you seen Kim K's latest ad?");
+}
+//Define political variable; prompt user
 var poli = prompt("On November 2nd, was your feed filled with posts that said \n'I Voted'?");
-
+//Check for user input
+if(poli === "") {
+    var poli = prompt("We need your answer! On November 2nd, was your feed filled with posts that said \n'I Voted'?");
+}
+//If user answered yes to both
 if(celeb === "yes" && poli === celeb){
     console.log("Congratulations! You have a well-rounded Facebook feed.");
-
+//if user answered no to both
 }if(celeb === "no" && poli === celeb){
     console.log("Your Facebook friends are quite inactive.");}
-
+//if user answered yes to the first question and no to the second
 if(celeb === "yes" && poli != celeb){
     console.log("Your feed is full of celebrity news!");
 }
+//if user answered no to the first question and yes to the second
 if(celeb === "no" && poli != celeb){
     console.log("You've got some real politicos in your friends' list!");
 }
