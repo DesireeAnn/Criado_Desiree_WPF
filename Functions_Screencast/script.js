5a883aa14b9a9674eaa4c897723eae3ce20e7fdf
DesireeAnn <desireeacriado@gmail.com>
2014-11-19 09:36:45 -0800

    Third functions screencast
    Variable scope in functions

--- Functions_Screencast/script.js
+++ Functions_Screencast/script.js
@@ -3,13 +3,9 @@
 function functionName (){
     //code the function runs
 }
-*/
-//Function Execution
 
+//Functions - Execution
 
-function outptMsg(){
-    console.log("Hello World");
-}
 //Function Definition
 function calcArea(){
     var width = 20;
@@ -20,4 +16,18 @@ function calcArea(){
 //Function Call
 calcArea();
 calcArea();
+calcArea();
+*/
+//Functions - Variable Scope
+
+//Variable is outside function
+var width = 5;
+//Variable is scoped to function
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+//If we call the function, the variable scoped to function will be used.
 calcArea();
