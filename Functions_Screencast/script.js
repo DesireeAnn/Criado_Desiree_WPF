2d07790800e01cbd512386e7a44198d362b9cc24
DesireeAnn <desireeacriado@gmail.com>
2014-11-19 09:30:54 -0800

    Second Function Screencast
    Calling a function

--- Functions_Screencast/script.js
+++ Functions_Screencast/script.js
@@ -4,13 +4,20 @@ function functionName (){
     //code the function runs
 }
 */
+//Function Execution
+
 
 function outptMsg(){
     console.log("Hello World");
 }
+//Function Definition
 function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
     console.log(area);
 }
+//Function Call
+calcArea();
+calcArea();
+calcArea();
