92259e6b5765d64f0f19df6c26ca3b34c823d888
DesireeAnn <desireeacriado@gmail.com>
2014-11-19 14:23:33 -0800

    Fourth functions screencast
    Parameters and arguments in functions

--- Functions_Screencast/script.js
+++ Functions_Screencast/script.js
@@ -17,7 +17,7 @@ function calcArea(){
 calcArea();
 calcArea();
 calcArea();
-*/
+
 //Functions - Variable Scope
 
 //Variable is outside function
@@ -31,3 +31,19 @@ function calcArea(){
 }
 //If we call the function, the variable scoped to function will be used.
 calcArea();
+
+//Functions - Arguments and Parameters
+
+//Basic layout
+funcName (argument1, argument2);
+function funcName (parameter1, parameter2){
+    //code the function runs
+}
+*/
+function dogYears(age){//holds parameters
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears+ " years old.");
+}
+var age1 = 4;
+dogYears(6);//holds arguments
+dogYears(age1);
