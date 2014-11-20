8511bb38f6d29fb8f913a1a4b53b2997ed327913
DesireeAnn <desireeacriado@gmail.com>
2014-11-19 17:02:32 -0800

    Func. Worksheet Ex.1
    Calculate circumference example

---
+++ Functions_Worksheet/script.js
@@ -0,0 +1,14 @@
+/* Desiree Criado
+  11/19/14
+  Functions Worksheet
+ */
+
+//Circumference
+var calcCirc = function(radius, pi){
+    var circ = radius * pi;
+    return circ;
+}
+var a = calcCirc(2,3.14);
+console.log("The circumference of the circle is "+ a);
+
+//Stung!
