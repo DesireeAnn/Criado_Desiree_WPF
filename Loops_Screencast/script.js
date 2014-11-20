991d6893835d3b1c0ebfbd9d505ec3483ef5cb12
DesireeAnn <desireeacriado@gmail.com>
2014-11-19 17:18:05 -0800

    while loops
    Loops screencast - while loops

---
+++ Loops_Screencast/script.js
@@ -0,0 +1,8 @@
+//Loops - "while" loop
+
+var b = 99;//sets up the index
+
+while (b > 0){ //checks the condition
+    console.log(b + " kegs on the wall");
+    b--;//increments or decrements the index
+}
