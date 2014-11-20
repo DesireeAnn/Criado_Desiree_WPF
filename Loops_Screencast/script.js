a104baed3254050bbed6cee8abaff3542e3f256f
DesireeAnn <desireeacriado@gmail.com>
2014-11-19 17:20:50 -0800

    Do While loops
    Loops screencast - do while loops

--- Loops_Screencast/script.js
+++ Loops_Screencast/script.js
@@ -6,3 +6,12 @@ while (b > 0){ //checks the condition
     console.log(b + " kegs on the wall");
     b--;//increments or decrements the index
 }
+//Loops - "do while" loop
+
+var c = 10;
+
+do{
+    console.log(c + " kegs on the wall");
+    c--;
+}
+while(c > 0);
