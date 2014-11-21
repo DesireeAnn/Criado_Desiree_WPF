1658055c467fdd3a1362ad84a5d65a198edaecbf
DesireeAnn <desireeacriado@gmail.com>
2014-11-20 16:17:41 -0800

    Functions - Industry
    Basic layout for Industry assignment

--- Functions_Industry/script.js
+++ Functions_Industry/script.js
@@ -2,3 +2,18 @@
 11/20/14
 Functions - Industry
  */
+//Providing basic price quote for clients
+
+//Greet User
+alert("Thank you for your interest in our work! We'd be happy to provide you with a basic estimate.");
+
+//Prompt for Shirt or Sweater
+var style = prompt("Are you purchasing a shirt or sweater?");
+
+//Prompt for Front Only or Front and Back
+var sides = prompt("Design on Front Only or Front and Back?");
+
+//Prompt for Number of Colors based on previous answer
+if(sides = "Front Only"){
+    var colors = prompt("How many colors are in the design?");
+}else{var colors = prompt("How many colors on the front and back combined?");}
